a314d1cf950fd8345f75e384ad5ff07037ea9548
John Puddephatt <john@jdp.org.uk>
2019-05-12 12:55:31 +0100

    fix typo in category seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -17,8 +17,8 @@ class CategoriesTableSeeder extends Seeder
             array('label' => 'Older people', 'slug' => 'older-people'),
             array('label' => 'Sports', 'slug' => 'sports'),
             array('label' => 'Vulnerable people', 'slug' => 'vulnerable-people'),
-            array('label' => 'Women', 'slug' => 'women')
-            array('label' => 'Young people', 'slug' => 'young'),
+            array('label' => 'Women', 'slug' => 'women'),
+            array('label' => 'Young people', 'slug' => 'young')
         );
 
         DB::table('categories')->insert($data);
