b6022944a937cbf86669114d83a73cb4bdf626a9
John Puddephatt <john@jdp.org.uk>
2019-05-15 00:11:05 +0100

    revert; looks like json only supported in mysql 5.7++

--- database/migrations/2019_03_20_213426_create_opportunities_table.php
+++ database/migrations/2019_03_20_213426_create_opportunities_table.php
@@ -35,12 +35,12 @@ class CreateOpportunitiesTable extends Migration
             $table->integer('minimum_age')->nullable();
             $table->text('expenses')->nullable();
             $table->text('requirements')->nullable();
-            $table->json('skills_needed');
-            $table->json('skills_gained');
+            $table->jsonb('skills_needed')->nullable();
+            $table->jsonb('skills_gained')->nullable();
 
             // Location
-            $table->boolean('from_home');
-            $table->json('address');
+            $table->boolean('from_home')->default(false);
+            $table->jsonb('address')->nullable();
             $table->text('address_ward')->nullable();
             $table->double('longitude')->nullable();
             $table->double('latitude')->nullable();
