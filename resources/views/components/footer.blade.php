18ac145c8915902a10445afc2e4177952f31edf4
John Puddephatt <john@jdp.org.uk>
2019-07-26 17:17:49 +0100

    fix up footer privacy and terms links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,9 +3,8 @@
     <div class="primary-footer--left">
       <h3 class="footer-brand">Volunteer <svg xmlns="http://www.w3.org/2000/svg" width="20.2" height="15" viewbox="0 0 25.2 21"><path fill="#FFF" d="M.6 5.7L16 11.3 0 15.1 1.4 21l23.4-5.5.2-3.5.2-3.8L2.6 0z"/></svg> Wakefield</h3>
       <nav>
-        <a href="#">Terms and conditions</a>
-        <a href="#">Site privacy policy</a>
-        <a href="#">Cookie policy</a>
+        <a href="/terms">Terms and conditions</a>
+        <a href="/privacy">Site privacy policy</a>
       </nav>
     </div>
     <div class="primary-footer--right">
