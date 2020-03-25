874e234c18bd16978bd53ef483b273b3491b7b66
John Puddephatt <john@jdp.org.uk>
2020-03-25 10:06:02 +0000

    hide opportunities panel from covid groups

--- resources/views/organisation/dashboard.blade.php
+++ resources/views/organisation/dashboard.blade.php
@@ -49,40 +49,40 @@
             </div>
           @endif
         </div>
-      @endif
-
-      <div class="card opportunity-list">
-        <div class="card-header">
-          <div>
-            <h4 class="card-title">My opportunities</h4>
-            <a href="{{ route('opportunity.new') }}" class="button">Add new opportunity</a>
-          </div>
-        </div>
-        @if(isset($opportunities) && count($opportunities))
-          <ul class="list-group">
-            @foreach($opportunities as $opportunity)
-              <li class="list-group-item">
-                <div class="item-title">{{$opportunity->title}}</div>
-                @if($opportunity->expires_in())
-                  <span class="muted">{{$opportunity->expires_in() }} days remaining</span>
-                @else
-                  <span class="muted">Expired</span>
-                @endif
-                <div class="list-group-buttons">
-                  <a class="button button__small button__ghost" href="/opportunity/renew/{{$opportunity->hash() }}">Renew</a> <a class="button button__small button__ghost" href="/opportunity/delete/{{$opportunity->hash() }}">Delete</a>  <a class="button button__small" href="/opportunity/edit/{{$opportunity->hash() }}">Edit</a>
-                </div>
-              </li>
-            @endforeach
-          </ul>
-        @else
-          <div class="card-body centered">
-            <div class="placeholder">
-              <p>No opportunities added yet.</p>
-              <a href="{{ route('opportunity.new') }}" class="btn btn-primary">Add your first</a>
+      @else
+        <div class="card opportunity-list">
+          <div class="card-header">
+            <div>
+              <h4 class="card-title">My opportunities</h4>
+              <a href="{{ route('opportunity.new') }}" class="button">Add new opportunity</a>
             </div>
           </div>
-        @endif
-      </div>
+          @if(isset($opportunities) && count($opportunities))
+            <ul class="list-group">
+              @foreach($opportunities as $opportunity)
+                <li class="list-group-item">
+                  <div class="item-title">{{$opportunity->title}}</div>
+                  @if($opportunity->expires_in())
+                    <span class="muted">{{$opportunity->expires_in() }} days remaining</span>
+                  @else
+                    <span class="muted">Expired</span>
+                  @endif
+                  <div class="list-group-buttons">
+                    <a class="button button__small button__ghost" href="/opportunity/renew/{{$opportunity->hash() }}">Renew</a> <a class="button button__small button__ghost" href="/opportunity/delete/{{$opportunity->hash() }}">Delete</a>  <a class="button button__small" href="/opportunity/edit/{{$opportunity->hash() }}">Edit</a>
+                  </div>
+                </li>
+              @endforeach
+            </ul>
+          @else
+            <div class="card-body centered">
+              <div class="placeholder">
+                <p>No opportunities added yet.</p>
+                <a href="{{ route('opportunity.new') }}" class="btn btn-primary">Add your first</a>
+              </div>
+            </div>
+          @endif
+        </div>
+      @endif
 
 
   </div>
