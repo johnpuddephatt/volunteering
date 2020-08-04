f4cf30ccfe002fd9c5f2f9f45c2bf88de849c8d9
John Puddephatt <john@jdp.org.uk>
2020-08-04 15:57:46 +0100

    remove street marshals role

--- resources/views/organisation/index.blade.php
+++ resources/views/organisation/index.blade.php
@@ -6,15 +6,6 @@
 
     <h2 class="page-title">Your nearest centres</h2>
 
-
-    <div class="card organisation-card highlighted">
-      <div class="organisation-card--header">
-        <a class="button button__secondary opportunity-card--enquire-button" href="https://volunteerwakefield.org/opportunities/street-marshals-219">Find out more</a>
-        <h3 class="organisation-card--title">Street Marshals</h3>
-        <p class="organisation-card--subtitle">We're looking for volunteers to help us welcome people back in to our city centre</p>
-      </div>
-    </div>
-
     @foreach($organisations as $organisation)
       <div class="card organisation-card">
         <div class="organisation-card--header">
