c5f5ad83fafe60ee21ba7c9ed55bce992564a3e0
John Puddephatt <john@jdp.org.uk>
2021-04-27 17:49:30 +0100

    fix contact form

--- resources/views/enquiry/form.blade.php
+++ resources/views/enquiry/form.blade.php
@@ -15,20 +15,22 @@
       </div>
       <div class="card-body">
         <h3>Express your interest</h3>
+
         @if($enquirable)
           @if($enquirable->contact_phone)<div class="opportunity-sidebar--phone"><a href="tel:{{ $enquirable->contact_phone }}">{{ $enquirable->contact_phone }}</a></div>@endif
           @if($enquirable->contact_email)<div class="opportunity-sidebar--email"><a href="{{ $enquirable->contact_email }}">{{ $enquirable->contact_email }}</a></div>@endif
+          @if($enquirable->phone)<div class="opportunity-sidebar--phone"><a href="tel:{{ $enquirable->phone }}">{{ $enquirable->phone }}</a></div>@endif
+          @if($enquirable->email)<div class="opportunity-sidebar--email"><a href="{{ $enquirable->email }}">{{ $enquirable->email }}</a></div>@endif
         @endif
 
-
-
-        @if(!$enquirable || $enquirable->accepts_enquiries)
+        @if(!$enquirable || $enquirable->accepts_enquiries || $enquirable->organisation)
           <p>You can use the form below to send an enquiry.</p>
           @foreach ($errors->all() as $message)
             <div class="alert alert__warning">{{ $message }}</div>
           @endforeach
           {!! form($form, ['class'=>'opportunity-form']) !!}
         @endif
+
       </div>
     </div>
 
